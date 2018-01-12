2df98343b3bd00b38ec63e2d6b793bf085625458
Edyta Pawlak <edyta.pawlakk@gmail.com>
2018-01-12 18:31:26 +0100

    kody urzędów

--- src/convert.py
+++ src/convert.py
@@ -1,10 +1,14 @@
+import sys
 import xmltodict
 
-with open('../data/codes.xsd') as infile:
+in_path = sys.argv[1]
+out_path = sys.argv[2]
+
+with open(in_path) as infile:
     doc = xmltodict.parse(infile.read())
 
 lista = doc['xsd:schema']['xsd:simpleType']['xsd:restriction']['xsd:enumeration']
-with open('us_codes.dat', 'w') as outfile:
+with open(out_path, 'w') as outfile:
     for a in lista:
         outfile.write(a['@value'] + ' ' + a['xsd:annotation']['xsd:documentation'] + '\n')
 
